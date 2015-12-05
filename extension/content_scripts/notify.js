a176c9148930eae42fc9bf6d5b973c68d6816295
Florian Dold <florian.dold@gmail.com>
2015-12-05 16:04:25 +0100

    Make wallet handshake conform more to spec.

--- extension/content_scripts/notify.js
+++ extension/content_scripts/notify.js
@@ -3,18 +3,28 @@
 
 'use strict';
 
-// Listen to messages from the backend.
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    // do nothing, yet
-});
-
-if (document && document.body)
-{
+// Install our handshake handlers only once the
+// document is loaded
+// TODO: change the spec to do it on the body
+document.addEventListener("DOMContentLoaded", function(e) {
+  console.log("DOM fully loaded and parsed");
   document.body.addEventListener('taler-checkout-probe', function(e) {
     let evt = new Event('taler-wallet-present');
     document.body.dispatchEvent(evt);
+    console.log("merchant handshake done");
   });
-}
+  document.body.addEventListener('taler-wire-probe', function(e) {
+    let evt = new Event('taler-wallet-present');
+    document.body.dispatchEvent(evt);
+    console.log("bank handshake done");
+  });
+  document.body.addEventListener('taler-create-reserve', function(e) {
+    console.log("reserve creation " + JSON.stringify(e.detail));
+    chrome.runtime.sendMessage({action:'new-reserve-request', detail:e.detail}, function(resp) {
+      console.log("got response");
+      document.location.href = resp.url;
+    });
+  });
+});
 
 console.log("Taler wallet: content page loaded");
