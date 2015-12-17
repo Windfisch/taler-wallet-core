6d677f0ec531fc8caea487e916884b14a049891a
Florian Dold <florian.dold@gmail.com>
2015-12-17 13:37:57 +0100

    Explain purpose of timer thread.

--- extension/background/timerThread.js
+++ extension/background/timerThread.js
@@ -1,3 +1,10 @@
+/**
+ * This file should be used as a WebWorker.
+ * Background pages in the WebExtensions model do
+ * not allow to schedule callbacks that should be called
+ * after a timeout.  We can emulate this with WebWorkers.
+ */
+
 onmessage = function(e) {
   self.setInterval(() => postMessage(true), e.data.interval);
 }
