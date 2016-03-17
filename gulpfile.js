414d737b01c979c7e7993942a04c85dbed943b97
Florian Dold <florian.dold@gmail.com>
2016-03-17 20:39:28 +0100

    bump year

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 /*
  This file is part of TALER
- (C) 2015 GNUnet e.V.
+ (C) 2015-2016 GNUnet e.V.
 
  TALER is free software; you can redistribute it and/or modify it under the
  terms of the GNU General Public License as published by the Free Software
