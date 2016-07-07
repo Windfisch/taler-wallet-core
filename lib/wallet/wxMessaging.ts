1349ec561e34106b1ecdc149e7129fc49960f511
Marcello Stanisci <marcello.stanisci@inria.fr>
2016-07-07 17:59:29 +0200

    fixing #4594

--- lib/wallet/wxMessaging.ts
+++ lib/wallet/wxMessaging.ts
@@ -11,7 +11,7 @@
  A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License along with
- TALER; see the file COPYING.  If not, If not, see <http://www.gnu.org/licenses/>
+ TALER; see the file COPYING.  If not, see <http://www.gnu.org/licenses/>
  */
 
 
