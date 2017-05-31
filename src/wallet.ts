f5da3fcd1989b4cffeec72c28ce0dad3a586c31f
Florian Dold <florian.dold@gmail.com>
2017-05-31 17:10:12 +0200

    v0.2.3

--- src/wallet.ts
+++ src/wallet.ts
@@ -140,7 +140,7 @@ export class KeysJson {
   /**
    * List of paybacks for compromised denominations.
    */
-  @Checkable.List(Checkable.Value(Payback))
+  @Checkable.Optional(Checkable.List(Checkable.Value(Payback)))
   payback?: Payback[];
 
   /**
