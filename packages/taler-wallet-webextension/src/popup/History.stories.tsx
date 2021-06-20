c7f0e483232a01c1165c8146285b915f766dba78
Sebastian <sebasjm@gmail.com>
2021-06-20 20:54:58 -0300

    fix height in storybook

--- packages/taler-wallet-webextension/src/popup/History.stories.tsx
+++ packages/taler-wallet-webextension/src/popup/History.stories.tsx
@@ -37,7 +37,7 @@ export default {
       <link key="1" rel="stylesheet" type="text/css" href="/style/pure.css" />
       <link key="2" rel="stylesheet" type="text/css" href="/style/popup.css" />
       <link key="3" rel="stylesheet" type="text/css" href="/style/wallet.css" />
-      <div style={{ margin: "1em", width: 400, display: 'flex', padding: '0.5em', height: 'calc(20rem - 34px)', border: 'black solid 1px' }}>
+      <div style={{ margin: "1em", width: 400, display: 'flex', padding: '0.5em', height: 'calc(320px - 34px)', border: 'black solid 1px' }}>
         <Story />
       </div>
     </div>
