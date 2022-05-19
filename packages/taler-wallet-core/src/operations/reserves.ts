9dfef2100d421cce2ab7e540b6a7bfcea7bc6fd1
Florian Dold <florian@dold.me>
2022-05-19 13:30:04 +0200

    wallet-core: long-poll longer for reserve status

--- packages/taler-wallet-core/src/operations/reserves.ts
+++ packages/taler-wallet-core/src/operations/reserves.ts
@@ -538,7 +538,7 @@ async function updateReserve(
   }
 
   const reserveUrl = new URL(`reserves/${reservePub}`, reserve.exchangeBaseUrl);
-  reserveUrl.searchParams.set("timeout_ms", "200");
+  reserveUrl.searchParams.set("timeout_ms", "1000");
 
   const resp = await ws.http.get(reserveUrl.href, {
     timeout: getReserveRequestTimeout(reserve),
