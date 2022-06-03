459c1994e6d20c26c24254ae0cb36aa66926a5cb
Sebastian <sebasjm@gmail.com>
2022-06-03 15:56:58 -0300

    fix reference to css resources

---
+++ packages/anastasis-webui/src/main.ts
@@ -0,0 +1,29 @@
+import { setupI18n } from "@gnu-taler/taler-util";
+import { h, render } from "preact";
+import App from "./components/app";
+//import "./scss/main.scss";
+
+export default App;
+
+function main(): void {
+  try {
+    const container = document.getElementById("container");
+    if (!container) {
+      throw Error("container not found, can't mount page contents");
+    }
+    render(h(App, {}), container);
+  } catch (e) {
+    console.error("got error", e);
+    if (e instanceof Error) {
+      document.body.innerText = `Fatal error: "${e.message}".  Please report this bug at https://bugs.gnunet.org/.`;
+    }
+  }
+}
+
+// setupI18n("en", strings);
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", main);
+} else {
+  main();
+}
