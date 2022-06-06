a97b3abe21a5d21e048f817a57b828ca2f1d2845
Sebastian <sebasjm@gmail.com>
2022-06-06 11:46:49 -0300

    more copyright fixes

--- packages/anastasis-webui/src/pages/home/authMethod/AuthMethodIbanSolve.tsx
+++ packages/anastasis-webui/src/pages/home/authMethod/AuthMethodIbanSolve.tsx
@@ -1,3 +1,18 @@
+/*
+ This file is part of GNU Anastasis
+ (C) 2021-2022 Anastasis SARL
+
+ GNU Anastasis is free software; you can redistribute it and/or modify it under the
+ terms of the GNU Affero General Public License as published by the Free Software
+ Foundation; either version 3, or (at your option) any later version.
+
+ GNU Anastasis is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License along with
+ GNU Anastasis; see the file COPYING.  If not, see <http://www.gnu.org/licenses/>
+ */
 import { ChallengeInfo } from "@gnu-taler/anastasis-core";
 import { h, VNode } from "preact";
 import { useState } from "preact/hooks";
