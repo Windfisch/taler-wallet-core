31d6d352c11e83c85c123671b3c7bb19a34510b9
Sebastian <sebasjm@gmail.com>
2022-06-06 11:45:01 -0300

    fixed some copyright headers

--- packages/anastasis-webui/copyleft-header.js
+++ packages/anastasis-webui/copyleft-header.js
@@ -13,23 +13,3 @@
  You should have received a copy of the GNU Affero General Public License along with
  GNU Anastasis; see the file COPYING.  If not, see <http://www.gnu.org/licenses/>
  */
-declare module "*.css" {
-  const mapping: Record<string, string>;
-  export default mapping;
-}
-declare module "*.svg" {
-  const content: any;
-  export default content;
-}
-declare module "*.jpeg" {
-  const content: any;
-  export default content;
-}
-declare module "*.png" {
-  const content: any;
-  export default content;
-}
-declare module "jed" {
-  const x: any;
-  export = x;
-}
